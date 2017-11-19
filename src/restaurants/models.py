62ef522bffac4df6a57b05d45ff2232c8fc23827
Harsha Vykunta <harshavardhan.biit@gmail.com>
2017-11-19 02:01:14 -0800

    created slugs and pre/post save

--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.db.models.signals import pre_save, post_save
+from .utils import unique_slug_generator
 # Create your models here.
 
 
@@ -9,6 +10,25 @@ class RestaurantLocation(models.Model):
     category = models.CharField(max_length=120,null=True,blank=False)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(null=True,blank=True)
 
     def __str__(self):
         return self.name
+
+    @property
+    def title(self):
+        return self.name
+
+
+def rl_pre_save_receiver(sender, instance, *args, **kwargs):
+        if not instance.slug:
+            instance.slug = unique_slug_generator(instance)
+
+
+    # def rl_post_save_receiver(self, instance, created, *args, **kwargs):
+    #     if not instance.slug:
+    #         instance.slug = unique_slug_generator(instance)
+
+pre_save.connect(rl_pre_save_receiver, sender=RestaurantLocation)
+
+    # post_save.connect(rl_post_save_receiver,sender=RestaurantLocation)
