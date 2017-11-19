b0835976a618c439d0d0f6d846a0b4af0412ce4e
Harsha Vykunta <harshavardhan.biit@gmail.com>
2017-11-18 19:39:06 -0800

    implementing class based view

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,27 +1,45 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
 
 
 # Create your views here.
 # function based view
+#
+# def home(request):
+#     # return HttpResponse("hello")
+#     content = {
+#         "content": "hello request"
+#     }
+#     return render(request, template_name="home.html", context=content)  # response
+#
+#
+# def about(request):
+#     content = {
+#         "content": "about request"
+#     }
+#     return render(request, template_name="about.html", context=content)
+#
+#
+# def contact(request):
+#     content = {
+#         "content": "contact request"
+#     }
+#     return render(request, template_name="contact.html", context=content)
 
-def home(request):
-    # return HttpResponse("hello")
-    content = {
-        "content": "hello request"
-    }
-    return render(request, template_name="home.html", context=content)  # response
+
+# class based view
 
 
-def about(request):
-    content = {
-        "content": "about request"
-    }
-    return render(request, template_name="about.html", context=content)
+class HomeView(TemplateView):
 
+    template_name = 'home.html'
 
-def contact(request):
-    content = {
-        "content": "contact request"
-    }
-    return render(request, template_name="contact.html", context=content)
+    def get_context_data(self,*args, **kwargs):
+        context = super(HomeView,self).get_context_data(*args, **kwargs)
+        context = {
+            "content": "hello from class view"
+        }
+
+        return context
