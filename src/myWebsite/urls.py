9412778e7358fb8550a021f7531481936822b213
Harsha Vykunta <harshavardhan.biit@gmail.com>
2017-11-21 05:34:25 -0800

    created profiles

--- src/myWebsite/urls.py
+++ src/myWebsite/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^reset-password', PasswordResetView.as_view(), name='password_reset'),
     url(r'^restaurants/', include('restaurants.urls', namespace='restaurants')),
+    url(r'^u/', include('profiles.urls', namespace='profiles')),
     url(r'^items/', include('menus.urls', namespace='menus')),
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
