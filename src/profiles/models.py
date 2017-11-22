99e7c0a86ff7ba3a16ebcd3eb4814660287459f3
Harsha Vykunta <harshavardhan.biit@gmail.com>
2017-11-21 17:30:18 -0800

    created profile model enabling to add followers

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.conf import settings
+from django.db.models.signals import post_save
 # Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User)
+    followers = models.ManyToManyField(User, related_name='is_following', blank=True)
+    activated = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+def post_save_user_receiver(sender, instance, created, *args, **kwargs):
+    if created:
+        profile, is_created = Profile.objects.get_or_create(user=instance)
+        default_user_profile = Profile.objects.get_or_create(user__id=1)[0]
+        default_user_profile.followers.add(instance)
+        profile.followers.add(default_user_profile.user)
+
+
+post_save.connect(post_save_user_receiver, User)
