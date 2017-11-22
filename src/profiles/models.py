7b8f9b60767dd522e74bcd43b3ef8ce40bedd75c
Harsha Vykunta <harshavardhan.biit@gmail.com>
2017-11-22 01:12:23 -0800

    implemented follow and un-follow

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -6,12 +6,28 @@ from django.db.models.signals import post_save
 User = settings.AUTH_USER_MODEL
 
 
+class ProfileManager(models.Manager):
+
+    def toggle_follow(self, request_user, user_to_toggle):
+        profile_ = Profile.objects.get(user__username__iexact=user_to_toggle)
+        user = request_user
+        is_following = False
+        if user in profile_.followers.all():
+            profile_.followers.remove(user)
+        else:
+            profile_.followers.add(user)
+            is_following = True
+        return profile_, is_following
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User)
     followers = models.ManyToManyField(User, related_name='is_following', blank=True)
     activated = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    objects = ProfileManager()
 
     def __str__(self):
         return self.user.username
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,12 +1,22 @@
 from django.contrib.auth import get_user_model
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import DetailView, View
 from restaurants.models import RestaurantLocation
 from menus.models import Item
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Profile
 # Create your views here.
 
 User = get_user_model()
+
+
+class ProfileFollowToggle(LoginRequiredMixin, View):
+
+    def post(self, request, *args, **kwargs):
+        username_to_toggle = request.POST.get('username')
+        profile_, is_following = Profile.objects.toggle_follow(request.user, username_to_toggle)
+        return redirect(f"/u/{profile_.user.username}")
 
 
 class ProfileDetailView(DetailView):
@@ -22,6 +32,10 @@ class ProfileDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super(ProfileDetailView, self).get_context_data(**kwargs)
         user = self.get_object()
+        is_following = False
+        if user.profile in self.request.user.is_following.all():
+            is_following = True
+        context['is_following'] = is_following
         query = self.request.GET.get('q')
         item_exists = Item.objects.filter(user=user).exists()
         queryset = RestaurantLocation.objects.filter(owner=user).search(query)
