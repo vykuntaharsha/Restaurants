78cfb0524081ade52baf8a12b632e8af270e15b3
Harsha Vykunta <harshavardhan.biit@gmail.com>
2017-11-22 03:55:52 -0800

    created register form and updated the redirect links

--- src/myWebsite/urls.py
+++ src/myWebsite/urls.py
@@ -16,14 +16,16 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
-from django.contrib.auth.views import LoginView,PasswordResetView
-from profiles.views import ProfileFollowToggle
+from django.contrib.auth.views import LoginView,PasswordResetView, LogoutView
+from profiles.views import ProfileFollowToggle, RegisterView
 from menus.views import HomeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^register/$', RegisterView.as_view(), name='register'),
     url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^logout/$', LogoutView.as_view(), name='logout'),
     url(r'^reset-password', PasswordResetView.as_view(), name='password_reset'),
     url(r'^profile-follow/$', ProfileFollowToggle.as_view(), name='follow'),
     url(r'^restaurants/', include('restaurants.urls', namespace='restaurants')),
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,14 +1,27 @@
 from django.contrib.auth import get_user_model
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import DetailView, View
+from django.views.generic import CreateView, DetailView, View
 from restaurants.models import RestaurantLocation
 from menus.models import Item
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Profile
+from .forms import RegisterForm
 # Create your views here.
 
 User = get_user_model()
+
+
+class RegisterView(CreateView):
+    form_class = RegisterForm
+    template_name = "registration/register.html"
+    success_url = '/'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return redirect("/logout")
+        return super(RegisterView, self).dispatch(*args, **kwargs)
+
 
 class ProfileFollowToggle(LoginRequiredMixin, View):
 
